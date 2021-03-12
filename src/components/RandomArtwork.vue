9a4557de8bd83878b8c9ce04443c8e502cd4bb8d
Andrew Ettensohn <andrewettensohn@gmail.com>
2021-03-12 14:56:39 -0500

    Make it prettier

--- src/components/RandomArtwork.vue
+++ src/components/RandomArtwork.vue
@@ -1,13 +1,17 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col>
-        <p>Random Art</p>
-        <v-img
-          max-height="150"
-          max-width="250"
-          :src="artwork.primaryImage"
-        ></v-img>
+  <v-container fluid>
+    <v-row class="text-center" justify="center">
+      <v-col class="text-center">
+        <div class="subheading font-weight-bold">{{ artwork.title }}</div>
+        <div v-if="artwork.artistDisplayName">
+          By {{ artwork.artistDisplayName }}
+        </div>
+        <div v-if="!artwork.artistDisplayName">Unknown Author</div>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="8">
+        <v-img contain height="800" :src="artwork.primaryImage"></v-img>
       </v-col>
     </v-row>
   </v-container>
@@ -53,7 +57,6 @@ export default {
       var collectionIds = await this.fetchCollectionIds();
       var id = this.getRandomElementFromArray(collectionIds);
       this.artwork = await this.fetchArtworkObjectById(id);
-      console.log(this.artwork);
     },
   },
 };
