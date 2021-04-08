2badeedbf3a7bbc196304920070474aa3c0491e0
Andrew Ettensohn <andrewettensohn@gmail.com>
2021-04-08 18:55:08 -0400

    add PWA plugin

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import VueMeta from 'vue-meta';
 import router from './router'
+import './registerServiceWorker'
 
 Vue.use(VueMeta);
 Vue.config.productionTip = false
