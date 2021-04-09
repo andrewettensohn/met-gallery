f24113a3cf4faf3924ef6e66dc1d5115599ba25d
Andrew Ettensohn <andrewettensohn@gmail.com>
2021-04-08 20:03:34 -0400

    second release

---
+++ precache-manifest.aadbf0d6d6c4f313489b87078d3e8e6a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2dc8de1f025fd187a28",
+    "url": "/met-gallery/css/app.601d2ada.css"
+  },
+  {
+    "revision": "c48d3bab064f8476b3a9",
+    "url": "/met-gallery/css/browse.bcc01e9e.css"
+  },
+  {
+    "revision": "70f4134e051089e46356",
+    "url": "/met-gallery/css/chunk-vendors.76529269.css"
+  },
+  {
+    "revision": "79bb9fa706b732d346ed7d975498605a",
+    "url": "/met-gallery/index.html"
+  },
+  {
+    "revision": "4f5fe8015f18303c139c",
+    "url": "/met-gallery/js/about.91d78196.js"
+  },
+  {
+    "revision": "e2dc8de1f025fd187a28",
+    "url": "/met-gallery/js/app.d46d39cb.js"
+  },
+  {
+    "revision": "c48d3bab064f8476b3a9",
+    "url": "/met-gallery/js/browse.50e4b31d.js"
+  },
+  {
+    "revision": "70f4134e051089e46356",
+    "url": "/met-gallery/js/chunk-vendors.fb760bbb.js"
+  },
+  {
+    "revision": "504576a6ace28533eb64e471f7ce714f",
+    "url": "/met-gallery/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/met-gallery/robots.txt"
+  }
+]);
